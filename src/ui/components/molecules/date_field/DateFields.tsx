0ea512dbe1fa25bcb207e7b3d2aed951df8d750a
Hamdan Shaikh <blacklisted.615@gmail.com>
2026-06-30 13:05:27 +0500

    fix calendar images

--- src/ui/components/molecules/date_field/DateFields.tsx
+++ src/ui/components/molecules/date_field/DateFields.tsx
@@ -1,214 +1,193 @@
-import { COLORS, EVENT, FONT_SIZE, SPACE } from 'config'
+import { COLORS, EVENT, FONT_SIZE, SPACE } from 'config';
 import React, {
-    FC,
-    useCallback,
-    useEffect,
-    useMemo,
-    useRef,
-    useState,
-} from 'react'
-import { Image, Pressable, StyleSheet, View } from 'react-native'
-import { AppLabel } from 'ui/components/atoms/app_label/AppLabel'
-import DateTimePickerModal from 'react-native-modal-datetime-picker'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { formatDate } from 'utils/Util'
-import { AttendanceLeaveApiRequestModel } from 'models/api_requests/AttendanceLeaveApiRequestModel'
-import EventBus from 'react-native-event-bus'
-import useEffectWithSkipFirstTime from 'hooks/useEffectWithSkipFirstTime'
+  FC,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import { Image, Pressable, StyleSheet, View } from 'react-native';
+import { AppLabel } from 'ui/components/atoms/app_label/AppLabel';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { formatDate } from 'utils/Util';
+import { AttendanceLeaveApiRequestModel } from 'models/api_requests/AttendanceLeaveApiRequestModel';
+import EventBus from 'react-native-event-bus';
+import useEffectWithSkipFirstTime from 'hooks/useEffectWithSkipFirstTime';
 
 type Props = {
-    setDateFields: (startDate: string, endDate: string) => void
-    isOpenFromRequestView?: boolean
-}
+  setDateFields: (startDate: string, endDate: string) => void;
+  isOpenFromRequestView?: boolean;
+};
 
 const DateFields: FC<Props> = ({
-    setDateFields,
-    isOpenFromRequestView = false,
+  setDateFields,
+  isOpenFromRequestView = false,
 }) => {
-    const [showDatePicker, setShowDatePicker] = useState<boolean>(false)
-    const currentDate = useMemo(() => new Date(), [])
-    let d = new Date()
-    d.setMonth(d.getMonth() - 1)
-    const [previousMonthDate, setPreviousMonthDate] = useState<Date>(d)
-    const [currentMonthDate, setCurrentMonthDate] = useState<Date>(currentDate)
-    const currentDateRef = useRef<string>('Right')
-    const hideDatePicker = () => {
-        setShowDatePicker(false)
+  const [showDatePicker, setShowDatePicker] = useState<boolean>(false);
+  const currentDate = useMemo(() => new Date(), []);
+  let d = new Date();
+  d.setMonth(d.getMonth() - 1);
+  const [previousMonthDate, setPreviousMonthDate] = useState<Date>(d);
+  const [currentMonthDate, setCurrentMonthDate] = useState<Date>(currentDate);
+  const currentDateRef = useRef<string>('Right');
+  const hideDatePicker = () => {
+    setShowDatePicker(false);
+  };
+
+  const returnCallback = useCallback(
+    (date?: string) => {
+      isOpenFromRequestView
+        ? setDateFields(
+            formatDate(String(previousMonthDate), 'DD-MM-YYYY'),
+            formatDate(
+              String(date ? new Date(date) : currentMonthDate),
+              'DD-MM-YYYY',
+            ),
+          )
+        : setDateFields(
+            formatDate(String(previousMonthDate), 'MMM DD, YY'),
+            formatDate(String(currentMonthDate), 'MMM DD, YY'),
+          );
+    },
+    [isOpenFromRequestView, setDateFields, previousMonthDate, currentMonthDate],
+  );
+
+  useEffectWithSkipFirstTime(() => {}, [previousMonthDate, currentMonthDate]);
+
+  useEffect(() => {
+    returnCallback();
+  }, [currentDate, previousMonthDate, returnCallback]);
+
+  const handleConfirm = useCallback((date: Date) => {
+    hideDatePicker();
+    if (currentDateRef.current === 'Left') {
+      setPreviousMonthDate(date);
+    } else {
+      setCurrentMonthDate(date);
     }
+  }, []);
+  const fetchLeaveRequests = useCallback(
+    (payload: AttendanceLeaveApiRequestModel) => {
+      setCurrentMonthDate(new Date(payload.end_date));
+      returnCallback(payload.end_date);
+    },
+    [returnCallback],
+  );
+  useEffect(() => {
+    EventBus.getInstance().addListener(
+      EVENT.FETCH_REQUEST_AGAIN,
+      fetchLeaveRequests,
+    );
 
-    const returnCallback = useCallback(
-        (date?: string) => {
-            isOpenFromRequestView
-                ? setDateFields(
-                      formatDate(String(previousMonthDate), 'DD-MM-YYYY'),
-                      formatDate(
-                          String(date ? new Date(date) : currentMonthDate),
-                          'DD-MM-YYYY'
-                      )
-                  )
-                : setDateFields(
-                      formatDate(String(previousMonthDate), 'MMM DD, YY'),
-                      formatDate(String(currentMonthDate), 'MMM DD, YY')
-                  )
-        },
-        [
-            isOpenFromRequestView,
-            setDateFields,
-            previousMonthDate,
-            currentMonthDate,
-        ]
-    )
+    return () => EventBus.getInstance().removeListener(fetchLeaveRequests);
+  }, [fetchLeaveRequests]);
 
-    useEffectWithSkipFirstTime(() => {}, [previousMonthDate, currentMonthDate])
+  const safeAreaInset = useSafeAreaInsets();
+  return (
+    <>
+      <View style={styles.flexContainer}>
+        <Pressable
+          onPress={() => {
+            currentDateRef.current = 'Left';
+            setShowDatePicker(true);
+          }}
+          style={styles.dateContainer}
+        >
+          <Image
+            source={require('assets/images/ic_calendar.png')}
+            style={styles.dateIcon}
+          />
+          <AppLabel
+            style={styles.dateText}
+            text={formatDate(String(previousMonthDate), 'MMM DD, YY')}
+          />
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            currentDateRef.current = 'Right';
+            setShowDatePicker(true);
+          }}
+          style={[styles.dateContainer, { marginHorizontal: SPACE.md }]}
+        >
+          <Image
+            source={require('assets/images/ic_calendar.png')}
+            style={styles.dateIcon}
+          />
+          <AppLabel
+            style={styles.dateText}
+            text={formatDate(String(currentMonthDate), 'MMM DD, YY')}
+          />
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            returnCallback();
+          }}
+        >
+          <Image
+            source={require('assets/images/icon_refresh.png')}
+            style={{
+              width: 18,
+              height: 18,
+            }}
+          />
+        </Pressable>
+      </View>
 
-    useEffect(() => {
-        returnCallback()
-    }, [currentDate, previousMonthDate, returnCallback])
-
-    const handleConfirm = useCallback((date: Date) => {
-        hideDatePicker()
-        if (currentDateRef.current === 'Left') {
-            setPreviousMonthDate(date)
-        } else {
-            setCurrentMonthDate(date)
-        }
-    }, [])
-    const fetchLeaveRequests = useCallback(
-        (payload: AttendanceLeaveApiRequestModel) => {
-            setCurrentMonthDate(new Date(payload.end_date))
-            returnCallback(payload.end_date)
-        },
-        [returnCallback]
-    )
-    useEffect(() => {
-        EventBus.getInstance().addListener(
-            EVENT.FETCH_REQUEST_AGAIN,
-            fetchLeaveRequests
-        )
-
-        return () => EventBus.getInstance().removeListener(fetchLeaveRequests)
-    }, [fetchLeaveRequests])
-
-    const safeAreaInset = useSafeAreaInsets()
-    return (
-        <>
-            <View style={styles.flexContainer}>
-                <Pressable
-                    onPress={() => {
-                        currentDateRef.current = 'Left'
-                        setShowDatePicker(true)
-                    }}
-                    style={styles.dateContainer}
-                >
-                    {/* <Image
-                        source={require('assets/images/ic_calendar.png')}
-                        style={styles.dateIcon}
-                    /> */}
-                    <AppLabel
-                        style={styles.dateText}
-                        text={formatDate(
-                            String(previousMonthDate),
-                            'MMM DD, YY'
-                        )}
-                    />
-                </Pressable>
-                <Pressable
-                    onPress={() => {
-                        currentDateRef.current = 'Right'
-                        setShowDatePicker(true)
-                    }}
-                    style={[
-                        styles.dateContainer,
-                        { marginHorizontal: SPACE.md },
-                    ]}
-                >
-                    {/* <Image
-                        source={require('assets/images/ic_calendar.png')}
-                        style={styles.dateIcon}
-                    /> */}
-                    <AppLabel
-                        style={styles.dateText}
-                        text={formatDate(
-                            String(currentMonthDate),
-                            'MMM DD, YY'
-                        )}
-                    />
-                </Pressable>
-                <Pressable
-                    onPress={() => {
-                        returnCallback()
-                    }}
-                >
-                    {/* <Image
-                        source={require('assets/images/icon_refresh.png')}
-                        style={{
-                            width: 18,
-                            height: 18,
-                        }}
-                    /> */}
-                </Pressable>
-            </View>
-
-            {showDatePicker && (
-                <DateTimePickerModal
-                    isVisible={true}
-                    mode="date"
-                    date={
-                        currentDateRef.current === 'Left'
-                            ? previousMonthDate
-                            : currentMonthDate
-                    }
-                    onConfirm={handleConfirm}
-                    onCancel={hideDatePicker}
-                    maximumDate={
-                        currentDateRef.current === 'Right' &&
-                        isOpenFromRequestView
-                            ? new Date(
-                                  new Date().setFullYear(
-                                      new Date().getFullYear() + 1
-                                  )
-                              )
-                            : new Date()
-                    }
-                    modalStyleIOS={{
-                        marginBottom:
-                            safeAreaInset.bottom > 0
-                                ? safeAreaInset.bottom
-                                : SPACE.lg,
-                    }}
-                />
-            )}
-        </>
-    )
-}
-export default DateFields
+      {showDatePicker && (
+        <DateTimePickerModal
+          isVisible={true}
+          mode="date"
+          date={
+            currentDateRef.current === 'Left'
+              ? previousMonthDate
+              : currentMonthDate
+          }
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+          maximumDate={
+            currentDateRef.current === 'Right' && isOpenFromRequestView
+              ? new Date(new Date().setFullYear(new Date().getFullYear() + 1))
+              : new Date()
+          }
+          modalStyleIOS={{
+            marginBottom:
+              safeAreaInset.bottom > 0 ? safeAreaInset.bottom : SPACE.lg,
+          }}
+        />
+      )}
+    </>
+  );
+};
+export default DateFields;
 const styles = StyleSheet.create({
-    flexContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        paddingHorizontal: SPACE.lg,
-        alignItems: 'center',
-        backgroundColor: COLORS.theme?.interface[50],
-        paddingBottom: SPACE.sm,
-        paddingTop: SPACE.xs,
-    },
-    dateIcon: {
-        width: 18,
-        height: 18,
-        alignSelf: 'center',
-    },
-    dateContainer: {
-        backgroundColor: COLORS.theme?.interface[200],
-        display: 'flex',
-        flexDirection: 'row',
-        paddingHorizontal: SPACE.sm,
-        paddingVertical: SPACE._2xs,
-        flex: 1,
-        borderRadius: 4,
-    },
-    dateText: {
-        fontSize: FONT_SIZE._2xs,
-        alignSelf: 'center',
-        marginLeft: SPACE.sm,
-    },
-})
+  flexContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    paddingHorizontal: SPACE.lg,
+    alignItems: 'center',
+    backgroundColor: COLORS.theme?.interface[50],
+    paddingBottom: SPACE.sm,
+    paddingTop: SPACE.xs,
+  },
+  dateIcon: {
+    width: 18,
+    height: 18,
+    alignSelf: 'center',
+  },
+  dateContainer: {
+    backgroundColor: COLORS.theme?.interface[200],
+    display: 'flex',
+    flexDirection: 'row',
+    paddingHorizontal: SPACE.sm,
+    paddingVertical: SPACE._2xs,
+    flex: 1,
+    borderRadius: 4,
+  },
+  dateText: {
+    fontSize: FONT_SIZE._2xs,
+    alignSelf: 'center',
+    marginLeft: SPACE.sm,
+  },
+});
--- src/ui/components/organisms/item_attendance/ItemAttendance.tsx
+++ src/ui/components/organisms/item_attendance/ItemAttendance.tsx
@@ -1,189 +1,176 @@
-import { COLORS, FONT_SIZE, SPACE } from 'config'
-import { AttendenceItem } from 'models/api_responses/AttendanceApiResponseModel'
-import ELeaveType from 'models/enums/ELeaveType'
-import React, { FC, useState } from 'react'
-import { ActivityIndicator, Image, StyleSheet, View } from 'react-native'
-import { AppLabel, TEXT_TYPE } from 'ui/components/atoms/app_label/AppLabel'
+import { COLORS, FONT_SIZE, SPACE } from 'config';
+import { AttendenceItem } from 'models/api_responses/AttendanceApiResponseModel';
+import ELeaveType from 'models/enums/ELeaveType';
+import React, { FC, useState } from 'react';
+import { ActivityIndicator, Image, StyleSheet, View } from 'react-native';
+import { AppLabel, TEXT_TYPE } from 'ui/components/atoms/app_label/AppLabel';
 // import HelpIcon from 'assets/images/help-icon.svg'
 
 type Props = {
-    item: AttendenceItem
-    index: number
-    lastIndex: number
-}
-    const ItemAttendance: FC<Props> = ({ item, index, lastIndex }) => {
-    const [onLoadImage, setOnloadImage] = useState(false)
-    const renderLocationImage = () => {
-         return (
-             <Image
-                 source={require('assets/images/ic_location.png')}
-                 style={styles.locationStyle}
-             />
-         )
-    }
-
-      const renderImage = () => {
-         return (
-             <View>
-                 {item.punch_out_id
-                     ? item.location_flag_punch_out === 1 &&
-                       renderLocationImage()
-                     : item.location_flag === 1 && renderLocationImage()}
-                 {item?.leave?.name === ELeaveType.EARLY_OFF ||
-                 item?.leave?.name === ELeaveType.BIRTHDAY ||
-                 item?.leave?.name === ELeaveType.URGENT ? (
-                     <HelpIcon height={55} width={55} />
-                 ) : (
-                     <>
-                         <Image
-                             source={{
-                                 uri:
-                                     item?.type === ELeaveType.ATTENDANCE
-                                         ? item.punch_out_id !== null
-                                             ? item?.punch_out_img['2x']
-                                             : item?.punch_in_img['2x']
-                                         : item?.leave?.image_url['2x'],
-                             }}
-                             onLoadEnd={() => {
-                                 setOnloadImage(false)
-                             }}
-                             onLoadStart={() => {
-                                 setOnloadImage(true)
-                             }}
-                             resizeMode={'cover'}
-                             style={{ width: 55, height: 55 }}
-                         />
-
-                         <ActivityIndicator
-                             size={'small'}
-                             animating={onLoadImage}
-                             color={COLORS.theme?.primaryColor}
-                             style={styles.activityIndicator}
-                         />
-                     </>
-                 )}
-             </View>
-         )
-      }
-
+  item: AttendenceItem;
+  index: number;
+  lastIndex: number;
+};
+const ItemAttendance: FC<Props> = ({ item, index, lastIndex }) => {
+  const [onLoadImage, setOnloadImage] = useState(false);
+  const renderLocationImage = () => {
     return (
-        <>
-            <View style={styles.topContainer}>
-                {/* {renderImage()} */}
-                <View style={styles.rightContainer}>
-                    <AppLabel
-                        style={{ fontSize: FONT_SIZE._3xs }}
-                        textType={TEXT_TYPE.SEMI_BOLD}
-                        text={item.date_formatted}
-                    />
+      <Image
+        source={require('assets/images/ic_location.png')}
+        style={styles.locationStyle}
+      />
+    );
+  };
 
-                    <View style={styles.bottomContainer}>
-                        {item?.type === ELeaveType.ATTENDANCE ? (
-                            <>
-                                <View style={styles.iconContainer}>
-                                    <Image
-                                        source={require('assets/images/icon_punch_in.png')}
-                                        style={styles.iconsStyle}
-                                    />
-                                    <AppLabel
-                                        style={styles.times}
-                                        text={item?.in_time}
-                                    />
-                                </View>
-                                <View style={styles.iconContainer}>
-                                    <Image
-                                        source={require('assets/images/icon_punch_out.png')}
-                                        style={styles.iconsStyle}
-                                    />
-                                    <AppLabel
-                                        style={styles.times}
-                                        text={item?.out_time}
-                                    />
-                                </View>
-                                <View style={styles.iconContainer}>
-                                    <Image
-                                        source={require('assets/images/punch_in_out.png')}
-                                        style={styles.iconsStyle}
-                                    />
-                                    <AppLabel
-                                        style={styles.times}
-                                        text={item?.working_hours}
-                                    />
-                                </View>
-                            </>
-                        ) : item?.leave?.name ? (
-                            <AppLabel
-                                text={
-                                    item.type === ELeaveType.HOLIDAY
-                                        ? `Holiday: ${item?.leave?.name}`
-                                        : `Absent: ${item?.leave?.name}`
-                                }
-                                style={{ fontSize: FONT_SIZE._3xs }}
-                            />
-                        ) : (
-                            <AppLabel
-                                text={'N/A'}
-                                style={{ fontSize: FONT_SIZE._3xs }}
-                            />
-                        )}
-                    </View>
+  const renderImage = () => {
+    return (
+      <View>
+        {item.punch_out_id
+          ? item.location_flag_punch_out === 1 && renderLocationImage()
+          : item.location_flag === 1 && renderLocationImage()}
+        {item?.leave?.name === ELeaveType.EARLY_OFF ||
+        item?.leave?.name === ELeaveType.BIRTHDAY ||
+        item?.leave?.name === ELeaveType.URGENT ? (
+          <HelpIcon height={55} width={55} />
+        ) : (
+          <>
+            <Image
+              source={{
+                uri:
+                  item?.type === ELeaveType.ATTENDANCE
+                    ? item.punch_out_id !== null
+                      ? item?.punch_out_img['2x']
+                      : item?.punch_in_img['2x']
+                    : item?.leave?.image_url['2x'],
+              }}
+              onLoadEnd={() => {
+                setOnloadImage(false);
+              }}
+              onLoadStart={() => {
+                setOnloadImage(true);
+              }}
+              resizeMode={'cover'}
+              style={{ width: 55, height: 55 }}
+            />
 
-                    {index !== lastIndex && <View style={styles.separator} />}
+            <ActivityIndicator
+              size={'small'}
+              animating={onLoadImage}
+              color={COLORS.theme?.primaryColor}
+              style={styles.activityIndicator}
+            />
+          </>
+        )}
+      </View>
+    );
+  };
+
+  return (
+    <>
+      <View style={styles.topContainer}>
+        {/* {renderImage()} */}
+        <View style={styles.rightContainer}>
+          <AppLabel
+            style={{ fontSize: FONT_SIZE._3xs }}
+            textType={TEXT_TYPE.SEMI_BOLD}
+            text={item.date_formatted}
+          />
+
+          <View style={styles.bottomContainer}>
+            {item?.type === ELeaveType.ATTENDANCE ? (
+              <>
+                <View style={styles.iconContainer}>
+                  <Image
+                    source={require('assets/images/icon_punch_in.png')}
+                    style={styles.iconsStyle}
+                  />
+                  <AppLabel style={styles.times} text={item?.in_time} />
                 </View>
-            </View>
-        </>
-    )
-}
-export default ItemAttendance
+                <View style={styles.iconContainer}>
+                  <Image
+                    source={require('assets/images/icon_punch_out.png')}
+                    style={styles.iconsStyle}
+                  />
+                  <AppLabel style={styles.times} text={item?.out_time} />
+                </View>
+                <View style={styles.iconContainer}>
+                  <Image
+                    source={require('assets/images/punch_in_out.png')}
+                    style={styles.iconsStyle}
+                  />
+                  <AppLabel style={styles.times} text={item?.working_hours} />
+                </View>
+              </>
+            ) : item?.leave?.name ? (
+              <AppLabel
+                text={
+                  item.type === ELeaveType.HOLIDAY
+                    ? `Holiday: ${item?.leave?.name}`
+                    : `Absent: ${item?.leave?.name}`
+                }
+                style={{ fontSize: FONT_SIZE._3xs }}
+              />
+            ) : (
+              <AppLabel text={''} style={{ fontSize: FONT_SIZE._3xs }} />
+            )}
+          </View>
+
+          {index !== lastIndex && <View style={styles.separator} />}
+        </View>
+      </View>
+    </>
+  );
+};
+export default ItemAttendance;
 const styles = StyleSheet.create({
-    topContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        paddingTop: SPACE.md,
-        justifyContent: 'center',
-    },
-    iconsStyle: {
-        width: 17,
-        height: 17,
-        tintColor: COLORS.theme?.interface[500],
-        marginEnd: SPACE.xs,
-    },
-    times: {
-        fontSize: FONT_SIZE._2xs,
-    },
-    separator: {
-        borderColor: COLORS.theme?.interface[200],
-        borderWidth: 0.5,
-        marginTop: SPACE.lg,
-    },
-    iconContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        flex: 1,
-    },
-    bottomContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: SPACE._2md,
-    },
-    rightContainer: {
-        flex: 1,
-        paddingHorizontal: SPACE.sm,
-        justifyContent: 'center',
-    },
-    locationStyle: {
-        width: 15,
-        height: 15,
-        position: 'absolute',
-        zIndex: 1,
-        top: 0,
-        right: 0,
-    },
-    activityIndicator: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0,
-    },
-})
+  topContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingTop: SPACE.md,
+    justifyContent: 'center',
+  },
+  iconsStyle: {
+    width: 17,
+    height: 17,
+    tintColor: COLORS.theme?.interface[500],
+    marginEnd: SPACE.xs,
+  },
+  times: {
+    fontSize: FONT_SIZE._2xs,
+  },
+  separator: {
+    borderColor: COLORS.theme?.interface[200],
+    borderWidth: 0.5,
+    marginTop: SPACE.lg,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: SPACE._2md,
+  },
+  rightContainer: {
+    flex: 1,
+    paddingHorizontal: SPACE.sm,
+    justifyContent: 'center',
+  },
+  locationStyle: {
+    width: 15,
+    height: 15,
+    position: 'absolute',
+    zIndex: 1,
+    top: 0,
+    right: 0,
+  },
+  activityIndicator: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
+});
